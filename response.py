7ef23349def220c78d07589cbcc07af6965db75e
amancodeit94 <amancodeit94@gmail.com>
2018-06-20 14:47:19 +0530

    Update response.py

--- response.py
+++ response.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-# vim: set et sw=4 ts=4 sts=4 ff=unix fenc=utf8:
-# Author: Binux<i@binux.me>
-#         http://binux.me
-# Created on 2012-11-02 11:16:02
 
 import cgi
 import re
