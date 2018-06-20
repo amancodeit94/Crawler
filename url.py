ab5e6159db7c63f90c3787afeeb6f7bb22c9d7d5
amancodeit94 <amancodeit94@gmail.com>
2018-06-20 14:50:46 +0530

    Update url.py

--- url.py
+++ url.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-# vim: set et sw=4 ts=4 sts=4 ff=unix fenc=utf8:
-# Author: Binux<i@binux.me>
-#         http://binux.me
-# Created on 2012-11-09 14:39:57
 
 import mimetypes
 
@@ -34,29 +28,6 @@ def _build_url(url, _params):
     netloc = netloc.encode('idna').decode('utf-8')
     if not path:
         path = '/'
-
-    if six.PY2:
-        if isinstance(scheme, six.text_type):
-            scheme = scheme.encode('utf-8')
-        if isinstance(netloc, six.text_type):
-            netloc = netloc.encode('utf-8')
-        if isinstance(path, six.text_type):
-            path = path.encode('utf-8')
-        if isinstance(params, six.text_type):
-            params = params.encode('utf-8')
-        if isinstance(query, six.text_type):
-            query = query.encode('utf-8')
-        if isinstance(fragment, six.text_type):
-            fragment = fragment.encode('utf-8')
-
-    enc_params = _encode_params(_params)
-    if enc_params:
-        if query:
-            query = '%s&%s' % (query, enc_params)
-        else:
-            query = enc_params
-    url = (urlunparse([scheme, netloc, path, params, query, fragment]))
-    return url
 
 
 def quote_chinese(url, encodeing="utf-8"):
@@ -99,25 +70,7 @@ def curl_to_arguments(curl):
                 if len(key_value) == 2:
                     key, value = key_value
                     headers[key.strip()] = value.strip()
-            elif current_opt in ('-d', '--data'):
-                kwargs['data'] = part
-            elif current_opt in ('--data-binary'):
-                if part[0] == '$':
-                    part = part[1:]
-                kwargs['data'] = part
-            elif current_opt in ('-X', '--request'):
-                kwargs['method'] = part
-            else:
-                raise TypeError('Unknow curl option: %s' % current_opt)
-            current_opt = None
-
-    if not urls:
-        raise TypeError('curl: no URL specified!')
-    if current_opt:
-        raise TypeError('Unknow curl option: %s' % current_opt)
-
-    kwargs['urls'] = urls
-    if headers:
+           
         kwargs['headers'] = headers
 
     return kwargs
